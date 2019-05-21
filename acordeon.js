299240ec22830cffecedd000d66e58607a3ea92b
stoyechounadan <stoyechounadan@hotmail.com>
2019-05-21 14:01:02 +0200

    sin los div

---
+++ acordeon.js
@@ -0,0 +1,12 @@
+
+var cuadrado= document.getElementById("cartel");
+
+cuadrado.addEventListener("click", agrandar);
+
+function agrandar(){
+
+cuadrado.style.width="300px";
+cuadrado.style.height="300px";
+}
+
+
